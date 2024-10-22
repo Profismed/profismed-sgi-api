e2b4813ee86ca76d57a437a92d5195dff2245b52
Nicolas Gil <nikosgil12@gmail.com>
2024-10-21 21:07:27 -0500

    .env added to gitignore

--- db/db-connection.js
+++ db/db-connection.js
@@ -1,9 +1,21 @@
-import mysql from 'mysql2/promise.js'
+import { Sequelize } from 'sequelize'
+
 import { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD } from '../config/config.js'
 
-export const pool = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  database: DB_NAME,
-  password: DB_PASSWORD
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+ host: DB_HOST,
+ dialect: 'mysql'
+})
+
+const tryConnection = async () => {
+ try {
+   await sequelize.authenticate()
+   console.log('Connection has been established successfully.')
+ } catch (error) {
+   console.error('Unable to connect to the database:', error)
+ }
+}
+
+tryConnection().then(() => {
+ console.log('Connection to database established')
 })
