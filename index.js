fd808a4e19500930e0f897965f8e521964271fc3
Nicolas Gil <nikosgil12@gmail.com>
2024-10-27 21:14:01 -0500

    cors added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import { PORT } from './config/config.js'
 import morgan from 'morgan'
 import { router as usersAuthRoutes } from './routes/users/users-auth-router.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use('/api/auth', usersAuthRoutes)
 
