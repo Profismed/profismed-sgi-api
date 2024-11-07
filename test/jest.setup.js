d990f6baf039424b9f2368c6e2104f796b9569e9
Nicolas Gil <nikosgil12@gmail.com>
2024-11-07 17:09:17 -0500

    jest setup updated for security

--- test/jest.setup.js
+++
@@ -1,8 +0,0 @@
-process.env.PORT = '3000'
-process.env.DB_PORT = '3306'
-process.env.DB_HOST = '149.130.164.6'
-process.env.DB_USER = 'profismed'
-process.env.DB_PASSWORD = 'qwerty21'
-process.env.DB_NAME = 'profismed_crm_db'
-process.env.JWT_SECRET = 'ultraprofisecreto'
-process.env.SALT_ROUNDS = '10'
