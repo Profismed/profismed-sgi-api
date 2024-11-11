07c125f4923fa9a7c982e74bcca10e4d19155d92
Nicolas Gil <nikosgil12@gmail.com>
2024-11-11 15:28:51 -0500

    users test written, initial version

--- test/users/users-auth.test.js
+++ test/users/users-auth.test.js
@@ -4,7 +4,7 @@ import { app } from '../../src/index.js';
 import { sequelize } from '../../src/db/db-connection.js';
 
 process.env.NODE_ENV = 'test';
-jest.useRealTimers(); // Usa temporizadores reales en lugar de fake timers para evitar conflictos con operaciones asíncronas
+jest.useRealTimers();
 
 let server;
 
@@ -19,7 +19,7 @@ describe('User Authentication', () => {
   });
 
   it('should return a 401 status and a message if the user already exists', async () => {
-    const response = await request(server) // Usa `server` para las pruebas
+    const response = await request(server)
       .post('/api/users/register')
       .send({
         username: 'janedoe',
@@ -38,5 +38,101 @@ describe('User Authentication', () => {
 
     expect(response.status).toBe(401);
     expect(response.body.message).toBe('Unauthorize');
+  });
+
+  it('TC-U001 Crear un nuevo usuario', async () => {
+    const response = await request(server)
+      .post('/api/users/create')
+      .send({
+        username: 'janedoe',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        userEmail: 'janedoe2@example.com',
+        userPhone: '+9876543210',
+        roleId: 3,
+        documentId: 2,
+        documentNumber: '987654321',
+        userJob: 'Project Manager',
+        userContactOrigin: 'Company Website',
+        locationId: 1,
+        password: 'anotherSecurePassword456',
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBeDefined();
+  });
+
+  it('TC-U002 Crear un usuario con datos incompletos', async () => {
+    const response = await request(server)
+      .post('/api/users/create')
+      .send({
+        lastName: 'Doe',
+        userEmail: 'janedoe@example.com',
+        userPhone: '+9876543210',
+        documentId: 2,
+        userContactOrigin: 'Company Website',
+        locationId: 1
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+  });
+
+  it('TC-U003 Iniciar sesión con credenciales válidas', async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'janedoe',
+        password: 'anotherSecurePassword456',
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.headers['set-cookie']).toBeDefined();
+  });
+
+  it('TC-U004 Iniciar sesión con credenciales inválidas', async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'janedoe',
+        password: 'securePassword456', // Contraseña incorrecta
+      });
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBeDefined();
+  });
+
+  it('TC-U005 Obtener todos los usuarios', async () => {
+    const response = await request(server)
+      .get('/api/users/all');
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    if (response.body.length > 0) {
+      expect(response.body[0]).toHaveProperty('username');
+      expect(response.body[0]).toHaveProperty('firstName');
+      expect(response.body[0]).toHaveProperty('lastName');
+    }
+  });
+
+  it('TC-U006 Obtener un usuario existente', async () => {
+    const userId = 1; // Id del usuario que deseas obtener
+    const response = await request(server)
+      .get(`/api/users/${userId}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('username');
+    expect(response.body).toHaveProperty('firstName');
+    expect(response.body).toHaveProperty('lastName');
+    expect(response.body).toHaveProperty('userEmail');
+  });
+
+  it('TC-U007 Obtener un usuario que no existe', async () => {
+    const userId = 99; // Id de un usuario que no existe
+    const response = await request(server)
+      .get(`/api/users/${userId}`);
+
+    expect(response.status).toBe(404); // Verifica que el código de estado sea 404
+    expect(response.body.message).toBeDefined(); // Verifica que el mensaje de error esté definido
   });
 });
