04f737aeca0045745e5271ae81acb5ac8d903f85
Nicolas Gil <nikosgil12@gmail.com>
2024-11-13 17:24:03 -0500

    sale date added automatically

--- src/controllers/sales/sales-controller.js
+++ src/controllers/sales/sales-controller.js
@@ -1,20 +1,26 @@
 import { saveSale, getSalesDb } from '../../repositories/sales/sales-item-repository.js'
 
 export const createSale = async (req, res) => {
-  const { buyerId, sellerId, items } = req.body
+  const { buyerId, sellerId, items } = req.body;
+
+  const saleDate = new Date(new Date().getTime() - (5 * 60 * 60 * 1000)).toISOString();
+
   const sale = {
     buyerId,
     sellerId,
-    items
-  }
+    items,
+    saleDate
+  };
+
   try {
-    await saveSale(sale)
-    res.status(201).send('Sale created')
+    await saveSale(sale);
+    res.status(201).send('Sale created');
   } catch (e) {
-    console.error(e)
-    res.status(500).send('Something went wrong')
+    console.error(e);
+    res.status(500).send('Something went wrong');
   }
-}
+};
+
 
 export const listSales = async (req, res) => {
   try {
