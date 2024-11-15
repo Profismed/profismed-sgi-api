15bdce5c57c90fb5d488105f9c1eab234ea37c85
Nicolas Gil <nikosgil12@gmail.com>
2024-11-14 21:46:40 -0500

    code formatted and check on sales when it have 0 items

--- src/repositories/reports/zones/zones-reports-repository.js
+++ src/repositories/reports/zones/zones-reports-repository.js
@@ -1,5 +1,5 @@
-import { TopSalesZone } from '../../../models/reports/zones/top-sales-zone.js';
-import { LeastSalesZone } from '../../../models/reports/zones/least-sales-zone.js';
+import { TopSalesZone } from '../../../models/reports/zones/top-sales-zone.js'
+import { LeastSalesZone } from '../../../models/reports/zones/least-sales-zone.js'
 
 export const retrieveTopSalesZone = async () => {
   try {
@@ -9,10 +9,10 @@ export const retrieveTopSalesZone = async () => {
         'location_name',
         'total_sales'
       ]
-    });
+    })
   } catch (error) {
-    console.error('Error retrieving top sales zone:', error);
-    throw error;
+    console.error('Error retrieving top sales zone:', error)
+    throw error
   }
 }
 
@@ -24,9 +24,9 @@ export const retrieveLeastSalesZone = async () => {
         'location_name',
         'total_sales'
       ]
-    });
+    })
   } catch (error) {
-    console.error('Error retrieving least sales zone:', error);
-    throw error;
+    console.error('Error retrieving least sales zone:', error)
+    throw error
   }
 }
